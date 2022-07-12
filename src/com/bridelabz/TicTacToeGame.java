16f1a6bc9dc918b417e05f67327f9482e90dffd5
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-12 23:55:11 +0530

    choose a letter to play game

--- src/com/bridelabz/TicTacToeGame.java
+++ src/com/bridelabz/TicTacToeGame.java
@@ -1,7 +1,30 @@
 package com.bridelabz;
-
+import java.util.Scanner;
 public class TicTacToeGame {
+    static char[] board = new char[10];
+    static char userLetter;
+    static char computerLetter;
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
+        createEmptyBoard();
+        chooseLetter();
     }
+
+    private static void createEmptyBoard()
+    {
+        for(int index=1; index<board.length;index++)
+        {
+            board[index] = ' ';
+        }
+    }
+
+    private static void chooseLetter()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose a letter :: X or O : ");
+        userLetter = scanner.next().toUpperCase().charAt(0);
+        computerLetter = (userLetter == 'X') ? 'O' : 'X';
+    }
+
+
 }
