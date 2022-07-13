2d005e000d3466c8b5a669c76f6c9b6d12bc1e25
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-13 20:39:03 +0530

    added display board

--- src/com/bridelabz/TicTacToeGame.java
+++ src/com/bridelabz/TicTacToeGame.java
@@ -5,12 +5,11 @@ public class TicTacToeGame {
     static char userLetter;
     static char computerLetter;
 
-
     public static void main(String[] args) {
-
         System.out.println("Welcome to Tic Tac Toe Game");
         createEmptyBoard();
         chooseLetter();
+        showBoard();
     }
 
     private static void createEmptyBoard()
@@ -27,6 +26,15 @@ public class TicTacToeGame {
         System.out.println("Choose a letter :: X or O : ");
         userLetter = scanner.next().toUpperCase().charAt(0);
         computerLetter = (userLetter == 'X') ? 'O' : 'X';
+    }
+
+    private static void showBoard()
+    {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("----------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("----------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
     }
 
 
