98ac1bc47e96fb8a2a9ea12ad4e672781074cd0d
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-13 22:44:56 +0530

    check free space is available or not

--- src/com/bridelabz/TicTacToeGame.java
+++ src/com/bridelabz/TicTacToeGame.java
@@ -11,6 +11,7 @@ public class TicTacToeGame {
         chooseLetter();
         showBoard();
         playerTurn();
+        checkFreeSpace();
     }
 
     private static void createEmptyBoard()
@@ -53,6 +54,29 @@ public class TicTacToeGame {
         }
         System.out.println("Player choose:: " + playerMove);
         board[playerMove] = userLetter;
+    }
+
+    private static void checkFreeSpace()
+    {
+        boolean isSpaceAvailable = false;
+        int numOfFreeSpaces = 0;
+        for(int index=1;index<board.length;index++)
+        {
+            if((board[index] == ' '))
+            {
+                isSpaceAvailable = true;
+                numOfFreeSpaces++;
+            }
+        }
+        if(isSpaceAvailable == false)
+        {
+            System.out.println("Board is full! You can't make another move");
+            System.exit(0);
+        }
+        else
+        {
+            System.out.println("Free space is available! you have "+numOfFreeSpaces+ " moves left");
+        }
     }
 
 }
